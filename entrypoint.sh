2f563d7bae08639fdc9150082730e173230f6144
Damian Bisignano <damian.bisignano@getequiem.com>
2022-07-04 13:32:38 +1000

    feat: create repo if it doesn't exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ repository_name="${INPUT_REPOSITORY_NAME}"
 
 repository=$(aws codecommit get-repository --repository-name ${repository_name} || aws codecommit create-repository --repository-name ${repository_name})
 
-CodeCommitUrl=$(echo $repository | jq .repositoryMetadata.cloneUrlHttp)
+CodeCommitUrl=$(echo $repository | jq -r .repositoryMetadata.cloneUrlHttp)
 
 git config --global --add safe.directory /github/workspace
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
