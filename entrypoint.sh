facf98eb94694cb239eb004f76f5ca2d0d7fa891
lucapette <ciao@lucapette.me>
2025-01-21 10:41:00 +0100

    this will probably not work

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,19 @@ CodeCommitUrl=$(echo $repository | jq -r .repositoryMetadata.cloneUrlHttp)
 
 remote="sync"
 
-git config --global --add safe.directory /github/workspace
+mkdir /github/acto-analytics
+
+cp -R /github/workspace/analytics-python /github/acto-analytics
+
+git config --global --add safe.directory /github/acto-analytics
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git remote add ${remote} ${CodeCommitUrl}
 
-if [[ $(git lfs ls-files) ]]; then
-  git config --global --add safe.directory /github/workspace/.git
-  git lfs migrate export --include="*" --everything
-  git lfs uninstall
-fi
+cd /github/workspace/analytics-python
+
+git add .
+
+git commit -m "sync analytics"
 
 git push --force --set-upstream ${remote} $(git rev-parse --abbrev-ref HEAD)
